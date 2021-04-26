3dd84a3e4c4568fbaecc6303572eac365645dfbd
xuanthangdong1992 <71828695+xuanthangdong1992@users.noreply.github.com>
2021-04-27 01:12:07 +1000

    check

--- index/db_conn.php
+++ index/db_conn.php
@@ -2,14 +2,14 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$databasename = "group6_accommodation_assignment";
+$databasename = "group_accommodation_assignment";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $databasename);
 
 // Check connection
 // if connection fail
 if ($conn->connect_error) {
-  printf("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
   exit();
 }
 ?>
