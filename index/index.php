fc9694ad02c41d4568c73adfc10dabae081b575b
xuanthangdong1992 <71828695+xuanthangdong1992@users.noreply.github.com>
2021-05-02 23:12:34 +1000

    update

--- index/index.php
+++ index/index.php
@@ -93,7 +93,7 @@ include('session.php');
 	include ('login-register-modal.php');
 	?>
 
-<script type="text/javascript" src="../js/script.js">
+
 		<script type="text/javascript">
 			<?php if (isset($_POST['btn_register'])) { ?> 
 
@@ -104,9 +104,48 @@ include('session.php');
 			<?php } ?>
 
 
+			//validation search form
+			$(document).ready(function() {
+				$('form[id="search-accommodation-form"]').validate({
+					rules: {
+						city: 'required',
+						startDate: 'required',
+						endDate: 'required',
+						numberOfGuest: {
+							digits: true
+						}
+					},
+					messages: {
+						city: '<span class="error">This field is required</span>',
+						startDate: '<span class="error">This field is required</span>',
+						endDate: '<span class="error">This field is required</span>',
+						numberOfGuest: '<span class="error">This field is required</span>'
+					},
+					submitHandler: function(form) {
+						// use ajax to send request to server
+						var city = $('#city').val();
+						var startDate = $('#startDate').val();
+						var endDate = $('#endDate').val();
+						var numberOfGuest = $('#numberOfGuest').val();
+						$.ajax({
+							url: 'list-accommodation-process.php',
+							method: 'POST',
+							data: {
+								city: city,
+								startDate: startDate,
+								endDate: endDate,
+								numberOfGuest: numberOfGuest
+							},
+							// get success message from server
+							success: function(data) {
+							$('#accommodation_data_list').html(data);
+							}
+						});
+					}
+				});
+			});
+
 			
-
-
 			// validate start date and end date
 			var td = new Date();
 			var mm = td.getMonth() + 1;
