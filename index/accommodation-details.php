0df42cd3f5a99f5e4eab763eff87ec11c721018e
xuanthangdong1992 <71828695+xuanthangdong1992@users.noreply.github.com>
2021-05-07 14:09:10 +1000

    client see the review function

--- index/accommodation-details.php
+++ index/accommodation-details.php
@@ -12,16 +12,17 @@ include ('db_conn.php');
 	<link rel="stylesheet" href="../css/clientstyle.css">
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
 	<title>Accommodation Booking System - Group 6</title>
 </head>
 <body>
 	<!-- jQuery and Bootstrap Bundle (includes Popper) -->
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 	<!-- support call ajax -->
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<!-- validation plugin -->
 	<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
 
   <?php
@@ -66,7 +67,7 @@ include ('db_conn.php');
         <p><b>Price: <?php echo $row["price"];?> AUD</b></p>
         <p><b>Number of room:</b> <?php echo $row["number_of_room"];?></p>
         <p><b>Number of bathroom:</b> <?php echo $row["number_of_bathroom"];?></p>
-        <p><b>Smoke_allowed:</b> <?php echo $row["smoke_allowed"]==1?'Yes':'No';?></p>
+        <p><b>Smoke allowed:</b> <?php echo $row["smoke_allowed"]==1?'Yes':'No';?></p>
         <p><b>Garage:</b> <?php echo $row["garage"]==1?'Yes':'No';?></p>
         <p><b>Pet friendly:</b> <?php echo $row["pet_friendly"]==1?'Yes':'No';?></p>
         <p><b>Internet provided:</b> <?php echo $row["internet_provided"]==1?'Yes':'No';?></p>
@@ -76,13 +77,47 @@ include ('db_conn.php');
         <p><b>City:</b> <?php echo $row["city"];?></p>
         <p><b>Postal code:</b> <?php echo $row["postal_code"];?></p>
         <p><b>State:</b> <?php echo $row["state"];?></p>
-        <p><b>Max-guests-allowed:</b> <?php echo $row["max_guests_allowed"];?></p>
-        <p><b>Accommodation_rate:</b> <span class="fa fa-star checked"></span><?php echo $row["accommodation_rate"];?></p>
+        <p><b>Max guests allowed:</b> <?php echo $row["max_guests_allowed"];?></p>
         <div class="submit-button">
-			<button class="btn btn-primary" type="button" data-toggle="modal" data-target="#bookingModal" >Booking</button>
-		</div>
+          <button class="btn btn-primary" type="button" data-toggle="modal" data-target="#bookingModal" >Booking</button>
+        </div>
+        <hr class="solid">
+        <!-- Accommodation review and rating -->
+        <?php
+          $queryAccommodationReview = "SELECT * FROM accommodation_review
+          WHERE accommodation_id = '$accommodation_id'";
+          $resultAccommdationReview = mysqli_query($conn, $queryAccommodationReview);
+          $countReviews = mysqli_num_rows($resultAccommdationReview);
+
+        ?>
+        <p><b>Accommodation rate:</b> <i class="bi bi-star-fill" style="color: red;"></i> <?php echo $row["accommodation_rate"];?> (<?php echo $countReviews; ?> reviews)</p>
+        <!-- Review list   -->
+        <!-- end Accommodation details -->
+        <?php
+        //just show 5 reviews. If client want to see more, they can click to view all reviews button. 
+        $i = 0;
+        foreach($resultAccommdationReview as $review){
+        $i++;
+        ?>
+        <div class="card border-info mb-12">
+          <div class="card-header">User: <?php echo $review["client_id"] ?> </div>
+          <div class="card-body text-info">
+            <p class="card-text"><i class="bi bi-star-fill" style="color: red;"></i> <?php echo $review["rate"] ?></p>
+            <p class="card-title"><?php echo $review["comment"] ?></p>
+          </div>
+        </div><br>
+        <?php
+          if($i == 5){
+            break;
+          }
+          }
+        ?>
+        <div class="submit-button">
+          <button class="btn btn-primary" type="button" data-toggle="modal" data-target="#reviewModal" >View all <?php echo $countReviews; ?> reviews</button>
+        </div>
       </div>
-      <!-- end Accommodation details -->
+      
+      
 
     </div>
 
@@ -192,6 +227,39 @@ include ('db_conn.php');
 		</div>
 	</div>
 	<!-- End Booking Modal -->
+
+   <!-- review Modal -->
+<div class="modal fade" id="reviewModal" tabindex="-1" role="dialog" aria-labelledby="reviewModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-body">
+					<!-- booking form -->
+					<button type="button" class="close float-right btn" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+					<h1>All reviews</h1>
+            <!-- Get user information -->
+            <?php
+        foreach($resultAccommdationReview as $review){
+        ?>
+        <div class="card border-info mb-12">
+          <div class="card-header">User: <?php echo $review["client_id"] ?> </div>
+          <div class="card-body text-info">
+            <p class="card-text"><i class="bi bi-star-fill" style="color: red;"></i> <?php echo $review["rate"] ?></p>
+            <p class="card-title"><?php echo $review["comment"] ?></p>
+          </div>
+        </div><br>
+        <?php
+          }
+        ?>
+
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Booking Modal -->
+
+
 
 <!-- include login and register modal   -->
   <?php
